1a7f7878d3c47531a3849c63df273653ffb0c129
unknown <C:\Users\ASUS\AppData\Roaming\The Bat!>
2025-12-17 16:31:07 +0500

    dict vazifa

---
+++ 2.py
@@ -0,0 +1,69 @@
+# 1-masala
+# student = {
+#     "saman": 18,
+#     "muzaffar": 19,
+#     "islom": 20,
+#     "asomiddin": 14,
+#     "sobitjon": 17
+# }
+#
+# for name in student:
+#     if student[name] >= 18:
+#         print(name)
+
+
+
+
+# 2-masala
+
+# login = {
+#     "jeymsBond": "agent007",
+#     "tony_stark": "ironman101",
+#     "piterParker": "spider.12.12",
+#     "sherlok": "sher.l04"
+# }
+#
+# username = input("login kiriting: ")
+# parol = input("Parol kiriting: ")
+#
+# if username in login:
+#     if login[username] == parol:
+#         print("xisobga kirdingiz")
+#     else:
+#         print("parol xato")
+#
+# else:
+#     print("hisob topilmadi")
+
+
+# 3-masala
+
+# bir = ['S001', 'S002', 'S003', 'S004']
+# ikki= ['Adina Park', 'Leyton Marsh', 'Duncan Boyle', 'Saim Richards']
+# uch = [85, 98, 89, 92]
+#
+# bosh_list = []
+# for i in range(len(bir)):
+#     bosh_list.append({bir[i]: {ikki[i]:{uch[i]}}})
+# print(bosh_list)
+
+
+# 4-masala
+# list1 = [('yellow', 1), ('blue', 2), ('yellow', 3), ('blue', 4), ('red', 1)]
+# natija = {}
+# for n, v in list1:
+#     natija.setdefault(n, []).append(v)
+# print(natija)
+#
+
+# 5-masala
+# royxat = {
+#     'Math': [88, 89, 90],
+#     'Physics': [92, 94, 89],
+#     'Chemistry': [90, 87, 93]
+# }
+#
+# royxat['Math'][0] += 1
+# royxat['Math'][1] += 1
+# royxat['Math'][2] += 1
+# print(royxat)
